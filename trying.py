c5e7b4a7ad47ebf6ea5f33a7140df7bb0d959300
dylanmaciel <dylanm10@hotmail.com>
2019-06-29 22:44:58 -0700

    welp

--- trying.py
+++ trying.py
@@ -5,6 +5,7 @@
 #                                                                              #
 #------------------------------------------------------------------------------# 
 
+
 #------------------------------------------------------------------------------#
 #                                                                              #
 #  Loading relevant libraries                                                  #
@@ -13,3 +14,41 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+
+from PIL import Image
+
+
+
+# this changes the working directory
+import os 
+os.chdir('C:/Users/dylan/Documents/Recursion-Cellular-Image-Classification')
+
+
+#------------------------------------------------------------------------------#
+#                                                                              #
+#  Trying to read in the data.                                                 #
+#                                                                              #
+#------------------------------------------------------------------------------# 
+
+# reading the training meta data csv
+meta_train = pd.read_csv('train.csv')
+print(meta_train.shape)
+
+meta_train_controls = pd.read_csv('train_controls.csv')
+print(meta_train_controls.shape)
+
+#  the following imports one of the specified files 
+im = Image.open('./B02/B02_s1_w1.png')
+
+# so we need to use the cs files to specify the path when getting images
+# keep track of well_type
+
+# finding the image path
+
+for i in range(1, meta_train.shape[1]):
+    experiment = meta_train[i].experiment
+    well = meta_train[i].well
+    plate = meta_train[i].plate
+
+
